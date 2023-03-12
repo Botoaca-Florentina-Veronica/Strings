3185af24778f72f815b13b819f50e89b0d103d8b
vera-botoaca <126331242+vera-botoaca@users.noreply.github.com>
2023-03-12 13:28:36 +0200

    Update Remove_spaces.c

--- Remove_spaces.c
+++ Remove_spaces.c
@@ -1,4 +1,4 @@
-//Program in c to remove all multiple spaces in a text and replace it with single spaces
+//Program in c to remove all multiple spaces in a text and replace it with single spaces(needs redo)
 #include<stdio.h>
 #include<string.h>
 #include<string.h>
@@ -26,5 +26,50 @@ int main(void)
     input[n]='\0';  
     printf("%s\n", input);
     printf("\n");
+    return 0;
+}
+
+
+
+//Program in c to remove all multiple spaces in a text and replace it with single spaces
+#include<stdio.h>
+#include<ctype.h>
+int main(void)
+{
+
+    char input[100];
+    int i, j, n;
+
+    // Create a string to store resultant string
+    char result[100] = {0};
+
+    // get the string from the user
+    printf("Enter a string : ");
+    fgets(input, 100, stdin);
+    n=strlen(input);
+    // Iterate over the string - Remove extra spaces between the words
+    // index 'i' used for traversing the inputStr
+    // index 'j' used to update the resultStr
+    for(i=0, j=0; input[i]; i++)
+    {
+ 
+        // check if we got the whitespace.
+        if(isspace(input[i]) && isspace(input[i-1]))
+        {
+            // Two consecutive white spaces. Skip the 'i'. Continue to next iteration
+            continue;
+        }
+ 
+        // Found a valid character, update result. Increment 'j'
+        result[j++] = input[i];
+    }
+
+    // Add the terminating NULL character to resultStr
+    result[j] = '\0';
+
+    // Print Results
+    printf("Before: %s\n", input);
+    printf("After:  %s\n", result);
+
     return 0;
 }
