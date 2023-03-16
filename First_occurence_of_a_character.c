8663fcd226b093339b2617b6ebc30103cf88f65d
vera-botoaca <126331242+vera-botoaca@users.noreply.github.com>
2023-03-16 16:02:57 +0200

    Create First_occurence_of_a_character.c

---
+++ First_occurence_of_a_character.c
@@ -0,0 +1,27 @@
+//C program to find the first occurrence of a character in a string
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX_SIZE 100
+
+int main(void)
+{
+    int i, len;
+    char str[MAX_SIZE], ch;
+    int k=0;
+    printf("Introduceti un sir de caractere: ");
+    fgets(str, MAX_SIZE, stdin);
+    printf("Introduceti caractereul caruia doriti sa ii aflati indexul: ");
+    scanf("%c", &ch);
+    len=strlen(str);
+    for(i=len; i>=0; i--)
+    {
+        if(str[i]==ch)
+        {
+            k=i;   //retinem indexul in variabila k
+        }
+    }
+    printf("Primul index al caracterului %c este: %d", ch, k);
+    return 0;
+}
